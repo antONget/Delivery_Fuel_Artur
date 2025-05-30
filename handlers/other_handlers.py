be334c1169aeafc6bf52be65ed6a8562be59b6b1
antONget <a.l.ponomare@mail.ru>
2025-05-30 11:41:39 +0300

    обновление статуса заказа

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,18 +1,40 @@
 import asyncio
 
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.types import FSInputFile
-
+from database.requests import set_order_status
+from config_data.config import Config, load_config
 import logging
 
 router = Router()
+config: Config = load_config()
 
 
 @router.callback_query()
 async def all_callback(callback: CallbackQuery) -> None:
     logging.info(f'all_callback:{callback.data} {callback.from_user.id}')
     logging.info(callback.data)
+
+
+@router.message(F.text.startswith('/ordertocreate'))
+async def order(message: Message) -> None:
+    logging.info(f'order {message.text}')
+    list_admin = config.tg_bot.admin_ids.split(',')
+    if str(message.from_user.id) in list_admin:
+        if len(message.text.split(' ')) == 2:
+            order_id = message.text.split(' ', maxsplit=1)[-1]
+            if order_id.isdigit():
+                order_id = int(order_id)
+                await set_order_status(order_id=order_id, status='create')
+                await message.answer(text=f'Заказу № {order_id} присвоен статус CREATE')
+            else:
+                await message.answer(text='Номер заказа должен быть числом')
+        else:
+            await message.answer(text='Для применения команды необходимо через пробел указать номер заказа,'
+                                      ' например:/ordertocreate 100')
+    else:
+        await message.answer(text='Вам не доступна эта команда')
 
 
 @router.message()
